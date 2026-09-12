965f25e198e889160586dac86da8f517b3cf3218
Alexandre Loumi <alexandreloumi1995@gmail.com>
2026-09-12 10:48:40 +0200

    Created the games dataframe - Next step : add it to snowflake

--- scripts/ingest_games.py
+++ scripts/ingest_games.py
@@ -1,4 +1,15 @@
 import requests
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+password = os.getenv("SNOWFLAKE_PASSWORD")
+
+print(os.getenv("SNOWFLAKE_USER"))
+print(os.getenv("SNOWFLAKE_ACCOUNT"))
 
 response_col = requests.get("https://api-web.nhle.com/v1/club-schedule-season/COL/20252026")
 print(response_col.status_code)
@@ -8,25 +19,39 @@ data_col = response_col.json()
 games = []
 
 for game in data_col["games"]:
-    if game["gameType"] in [2, 3]:
+    if game["gameType"] == 2:
         games.append(game)
 
-print(len([game for game in games if game["gameType"] == 3]))
+print(len([game for game in games if game["gameType"] == 2]))
 
-print(len(games))
+# print(games[0])
 
-response_bracket = requests.get("https://api-web.nhle.com/v1/playoff-bracket/2026")
+print(games[0]["id"])
+print(games[0]["homeTeam"]["commonName"]["default"])
+print(games[0]["homeTeam"]["abbrev"])
+print(games[0]["homeTeam"]["score"])
+print(games[0]["awayTeam"]["commonName"]["default"])
+print(games[0]["awayTeam"]["abbrev"])
+print(games[0]["awayTeam"]["score"])
+print(games[0]["gameDate"])
 
-data_bracket = response_bracket.json()
 
-print(response_bracket.status_code)
 
-print(data_bracket["series"][0])
+raw_games = []
 
-bracket_col = []
+for game in games:
+    raw_games.append({
+        "game_id": game["id"],
+        "home_team": game["homeTeam"]["commonName"]["default"],
+        "home_team_abbrev": game["homeTeam"]["abbrev"],
+        "home_team_score": game["homeTeam"]["score"],
+        "away_team": game["awayTeam"]["commonName"]["default"],
+        "away_team_abbrev": game["awayTeam"]["abbrev"],
+        "away_team_score": game["awayTeam"]["score"],
+        "game_date": game["gameDate"]
+    })
 
-for series in data_bracket["series"]:
-    if series["topSeedTeam"]["abbrev"] == "COL" or series["bottomSeedTeam"]["abbrev"] == "COL":
-        bracket_col.append(series)
+raw_games_df = pd.DataFrame(raw_games)
 
-print(bracket_col)
+print(raw_games_df.head())
+print(raw_games_df.info())
